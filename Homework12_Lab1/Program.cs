b56152048abb8b8e80a329719ba2af5b73e1acab
Emily Sierra <sierraemely@gmail.com>
2024-11-30 18:48:31 -0800

    Add project files.

---
+++ Homework12_Lab1/Program.cs
@@ -0,0 +1,37 @@
+﻿using static Delegate;
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+
+        //  Delegate :
+        //Create 2 methods and assign the method to the delegate and use the method via the delegate.
+
+        Delegate myDelegate = new Delegate();
+        MyDelegate myDelegate1 =  myDelegate.printMessage;
+        MyDelegate myDelegate2 =  myDelegate.printMessage2;
+        myDelegate1.Invoke();
+        myDelegate2.Invoke();
+
+
+
+    }
+}
+
+
+
+public class Delegate
+{
+    public delegate void MyDelegate();
+
+    public void printMessage()
+    {
+        Console.WriteLine("Printing method 1");
+    }
+
+    public void printMessage2()
+    {
+        Console.WriteLine("Printing method 2");
+    }
+}
